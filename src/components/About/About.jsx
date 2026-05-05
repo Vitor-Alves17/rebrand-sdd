a3cde8e50469453c3bc30ac894d89fcb7175ade5
Vitor-Alves17 <vitoralves3306@gmail.com>
2026-05-05 17:46:29 -0300

    wip: created about page

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,14 +1,17 @@
+import './style.scss'
 
 export const About = () => {
     return (
-        <>
+        <div className='about-container'>
             <div className="img-wrapper">
                 <img src="" alt=""/>
                 <img src="" alt=""/>
             </div>
             <section className="text-container">
                 <span>nossa essência</span>
-                <h2>Compromisso com a <span>Alta Performace</span></h2>
+                <div className="title-wrapper">
+                    <h2>Compromisso com a <span>Alta Performace</span></h2>
+                </div>
                 <p>Na Suporte de Domingo, acreditamos que a tecnologia deve ser uma
                     força cinética — algo que impulsiona, que cura informações e que
                     simplifica processos complexos, trazendo a clareza de uma manhã
@@ -18,12 +21,11 @@ export const About = () => {
                     não são apenas funcionais, mas que comunicam autoridade, clareza
                     e frescor. Não entregamos apenas código; entregamos curadoria
                     tecnológica acolhedora.</p>
-                <div className="line"></div>
                 <div className="statistic">
                     <p><b>150+</b> projetos</p>
                     <p><b>98%</b> eficiência</p>
                 </div>
             </section>
-        </>
+        </div>
     )
 }
